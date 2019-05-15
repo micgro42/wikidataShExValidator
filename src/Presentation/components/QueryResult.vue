7bb6c8743a522f251188d538b71c229a87283801
Michael Große <mic.grosse@googlemail.com>
2019-05-16 00:18:49 +0200

    Add bulma to enable basic styling
    we might end up using it via cdn

--- src/Presentation/components/QueryResult.vue
+++ src/Presentation/components/QueryResult.vue
@@ -2,9 +2,9 @@
 <div>
     <ul>
         <li v-for="entity in entities">
-            <a :href="entity.url">{{entity.url}}</a>  <span v-if="entity.status">{{entity.status}}</span>
+            <a class="is-success" :href="entity.url">{{entity.url}}</a>  <span class="is-warning" v-if="entity.status">{{entity.status}}</span>
             <ul v-if="entity.errors">
-                <li v-for="error in entity.errors">
+                <li class="has-text-danger" v-for="error in entity.errors">
                     {{error.type}}: {{error.property}}
                 </li>
             </ul>
@@ -14,6 +14,7 @@
 </template>
 
 <script>
+    import bulma from 'bulma/css/bulma.css';
     import {mapGetters} from 'vuex';
     export default {
         name: 'QueryResult',
