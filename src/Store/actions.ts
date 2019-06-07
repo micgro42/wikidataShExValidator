89b17a344e1cf55e53e4d1938bd489d2a03fcd44
Michael Große <michael.grosse@wikimedia.de>
2019-06-07 13:48:20 +0200

    Extract store actions into new file
    They were getting too big anyway

---
+++ src/Store/actions.ts
@@ -0,0 +1,81 @@
+import SparqlFetcher from '@/SparqlFetcher/SparqlFetcher';
+import SparqlFetcherRequest from '@/SparqlFetcher/SparqlFetcherRequest';
+import SparqlFetcherResponse from '@/SparqlFetcher/SparqlFetcherResponse';
+import EntityInterface from '@/Store/EntityInterface';
+import EntityValidator from '@/EntityValidator/EntityValidator';
+import EntityValidatorRequest from '@/EntityValidator/EntityValidatorRequest';
+import {ShExCStatus} from '@/Store/ShExCStatus';
+import ShExCParser from '@/ShExCParser/ShExCParser';
+import ShExCParserRequest from '@/ShExCParser/ShExCParserRequest';
+import * as log from 'loglevel';
+import { Commit } from 'vuex';
+import StateInterface from '@/Store/StateInterface';
+
+export default {
+    setQuery({commit, state}: {commit: Commit, state: StateInterface}, query: string) {
+        commit('setQuery', {query});
+        const sparqlFetcher = new SparqlFetcher();
+        sparqlFetcher.fetchItems(new SparqlFetcherRequest(query))
+            .then((resp: SparqlFetcherResponse) => {
+                commit('setQueryEntities', {
+                    entities: resp.entities.reduce((carry: EntityInterface, url: string) => {
+                        const entityID = url.match(/[QPL]\d+/);
+                        if (entityID === null || !entityID[0]) {
+                            // FIXME: use proper logging!
+                            /* tslint:disable-next-line */
+                            console.warn('unexpected match for', url, entityID);
+                            return carry;
+                        }
+                        carry[entityID[0]] = {url};
+                        return carry;
+                    }, {}),
+                });
+            })
+            .then(async () => {
+                if (Object.keys(state.QueryEntities).length === 0) {
+                    return;
+                }
+                if (state.ShExCParseError.message.length !== 0) {
+                    return;
+                }
+                const validator = new EntityValidator(state.ShemaParsed);
+                for (const [id, {url}] of Object.entries(state.QueryEntities)) {
+                    const response = await validator.validate(new EntityValidatorRequest(id, url));
+                    commit('setEntityData', {
+                        id,
+                        status: response.status,
+                        error: response.errorMessage,
+                    });
+                }
+            });
+    },
+    updateShExC({commit}: {commit: Commit}, url: string) {
+        commit('setShExCStatus', ShExCStatus.loading);
+
+        fetch(url)
+            .then((response) => response.text(), (error) => {
+                // ToDo: ensure html status and content type
+                commit('setShExCStatus', ShExCStatus.loadingError);
+                throw error;
+            })
+            .then((ShExCText) => {
+                commit('setShExCStatus', ShExCStatus.inProgress);
+                const parser = new ShExCParser();
+                const response = parser.parse(new ShExCParserRequest(ShExCText));
+
+                if (response.error) {
+                    commit('setShExCStatus', ShExCStatus.invalid);
+                    commit('setShExCError', {
+                        errorMessage: response.error.message,
+                        lineNo: response.error.lineNo,
+                    });
+                    commit('setParsedSchema', {});
+                } else {
+                    commit('setShExCStatus', ShExCStatus.valid);
+                    commit('clearShExCError');
+                    commit('setParsedSchema', response.parsedSchema);
+                }
+            })
+            .catch((error) => log.error(error));
+    },
+};
--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,17 +1,10 @@
-import * as log from 'loglevel';
 import Vue from 'vue';
 import Vuex from 'vuex';
-import EntityValidator from '@/EntityValidator/EntityValidator';
-import EntityValidatorRequest from '@/EntityValidator/EntityValidatorRequest';
-import SparqlFetcher from '@/SparqlFetcher/SparqlFetcher';
-import SparqlFetcherRequest from '@/SparqlFetcher/SparqlFetcherRequest';
-import SparqlFetcherResponse from '@/SparqlFetcher/SparqlFetcherResponse';
 import EntityInterface from '@/Store/EntityInterface';
 import { ShExCStatus } from '@/Store/ShExCStatus';
 import { ValidationStatus } from '@/Store/ValidationStatus';
-import ShExCParser from '../ShExCParser/ShExCParser';
-import ShExCParserRequest from '../ShExCParser/ShExCParserRequest';
 import StateInterface from '@/Store/StateInterface';
+import actions from './actions';
 
 Vue.use(Vuex);
 
@@ -71,72 +64,5 @@ export default new Vuex.Store({
             };
         },
     },
-    actions: {
-        setQuery({commit, state}, query: string) {
-            commit('setQuery', {query});
-            const sparqlFetcher = new SparqlFetcher();
-            sparqlFetcher.fetchItems(new SparqlFetcherRequest(query))
-                .then((resp: SparqlFetcherResponse) => {
-                    commit('setQueryEntities', {
-                        entities: resp.entities.reduce((carry: EntityInterface, url: string) => {
-                            const entityID = url.match(/[QPL]\d+/);
-                            if (entityID === null || !entityID[0]) {
-                                // FIXME: use proper logging!
-                                /* tslint:disable-next-line */
-                                console.warn('unexpected match for', url, entityID);
-                                return carry;
-                            }
-                            carry[entityID[0]] = {url};
-                            return carry;
-                        }, {}),
-                    });
-                })
-                .then(async () => {
-                    if (Object.keys(state.QueryEntities).length === 0) {
-                        return;
-                    }
-                    if (state.ShExCParseError.message.length !== 0) {
-                        return;
-                    }
-                    const validator = new EntityValidator(state.ShemaParsed);
-                    for (const [id, {url}] of Object.entries(state.QueryEntities)) {
-                        const response = await validator.validate(new EntityValidatorRequest(id, url));
-                        commit('setEntityData', {
-                            id,
-                            status: response.status,
-                            error: response.errorMessage,
-                        });
-                    }
-                });
-        },
-        updateShExC({commit}, url) {
-            commit('setShExCStatus', ShExCStatus.loading);
-
-            fetch(url)
-                .then((response) => response.text(), (error) => {
-                    // ToDo: ensure html status and content type
-                    commit('setShExCStatus', ShExCStatus.loadingError);
-                    throw error;
-                })
-                .then((ShExCText) => {
-                    commit('setShExCStatus', ShExCStatus.inProgress);
-                    const parser = new ShExCParser();
-                    const response = parser.parse(new ShExCParserRequest(ShExCText));
-
-                    if (response.error) {
-                        commit('setShExCStatus', ShExCStatus.invalid);
-                        commit('setShExCError', {
-                            errorMessage: response.error.message,
-                            lineNo: response.error.lineNo,
-                        });
-                        commit('setParsedSchema', {});
-                    } else {
-                        commit('setShExCStatus', ShExCStatus.valid);
-                        commit('clearShExCError');
-                        commit('setParsedSchema', response.parsedSchema);
-                    }
-                })
-                .catch((error) => log.error(error));
-        },
-    },
+    actions,
 });
