b9b564ee515d094ebf1c4f78090a042a25b0ae61
Michael Große <mic.grosse@googlemail.com>
2019-06-30 22:06:43 +0200

    Stricter eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,27 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    '@vue/prettier',
+    '@vue/typescript',
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+        allowHigherOrderFunctions: true,
+      },
+    ],
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
