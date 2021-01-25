1c0ec17c26bd446af264d09c7ab8b3f9de468224
Michael Große <mic.grosse@googlemail.com>
2021-01-25 16:39:26 +0100

    Add test to see if it reaches sonar

---
+++ tests/unit/EntityValidator.spec.ts
@@ -0,0 +1,17 @@
+import EntityValidator from '@/EntityValidator/EntityValidator';
+import EntityValidatorRequest from '@/EntityValidator/EntityValidatorRequest';
+import { ValidationStatus } from '@/Store/ValidationStatus';
+
+describe('EntityValidator', () => {
+  it('fails validating against empty schema', async () => {
+    const validator = new EntityValidator({});
+    const validationRequest = new EntityValidatorRequest(
+      'Q1',
+      'https://exmaple.com',
+    );
+
+    const validationResponse = await validator.validate(validationRequest);
+
+    expect(validationResponse.status).toBe(ValidationStatus.Nonconformant);
+  });
+});
